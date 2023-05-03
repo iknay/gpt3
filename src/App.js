e8541558c339a8bfacadcf7c7db9acccb8270925
unknown <yanceevillanueva16@gmail.com>
2023-05-03 20:03:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import 
+import React from 'react';
+import { Brand, CTA, Navbar } from './components';
+import { Footer, Blog, Features, Header, Possibility, WhatGPT3 } from './containers';
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Navbar/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
   )
 }
 
